8a1c9a22a5ca55ac641e3af3a2558cc990d3cb01
IblD <david.ibl@lv1871.de>
2018-11-22 14:20:54 +0100

    solution für ein paar operatoren

--- src/app/services/rxjsService.ts
+++ src/app/services/rxjsService.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { ReplaySubject, Observable, of, from, timer } from 'rxjs';
-import { delay, delayWhen, tap } from 'rxjs/operators';
+import { ReplaySubject, Observable, of, from, timer, merge, combineLatest, concat } from 'rxjs';
+import { delay, delayWhen, map, takeWhile, distinctUntilChanged, pairwise, repeat, take } from 'rxjs/operators';
 import { Marble } from '../model/marble';
 
 @Injectable()
@@ -10,7 +10,7 @@ export class RxjsService {
     private observableCount = 0;
 
     public constructor() {
-        of(null).pipe( delay(2000) ).subscribe(val => this.init());
+        of(null).pipe( delay(2000) ).subscribe(val => this.init6());
     }
 
     public getItems() {
@@ -21,8 +21,89 @@ export class RxjsService {
         const obs1 = this.createNumberMarbles(0, 5000, 10000);
         const obs2 = this.createCharMarbles(1000,  500, 5500);
 
-        const obs3 = of(new Marble(1));
+        const obs3 = merge(obs1, obs2).pipe(
+            map(x => {
+                const f = new Marble(x.text, 0);
+                f.observableId = 3;
+                return f;
+            }));
         const observables = [obs1, obs2, obs3];
+
+        this.item.next(observables);
+    }
+
+    private init2() {
+        const obs1 = this.createNumberMarbles(0, 5000, 10000);
+        const obs2 = this.createCharMarbles(1000,  500, 5500);
+
+        const obs3 = combineLatest(obs1, obs2, (a, b) => {
+                const f = new Marble(a.text + b.text, 0);
+                f.observableId = 3;
+                return f;
+            });
+        const observables = [obs1, obs2, obs3];
+
+        this.item.next(observables);
+    }
+
+    private init3() {
+        const obs1 = this.createNumberMarbles(0, 2000, 4000);
+        const obs2 = this.createCharMarbles(7000,  7000, 7000);
+
+        const obs3 = obs1.pipe(
+            takeWhile(m => m.text === 1));
+        const observables = [obs3];
+
+        this.item.next(observables);
+    }
+
+    private init4() {
+        const obs1 = this.createNumberMarbles(0, 2000, 4000, new Marble(1, 100), new Marble(1, 7000), new Marble(2, 6000));
+        const obs2 = this.createCharMarbles(7000,  7000, 7000);
+
+        const obs3 = obs1.pipe(
+            distinctUntilChanged((m, n) => m.text === n.text),
+            map(m => {
+                const f = new Marble(m.text, 0);
+                f.observableId = 3;
+                return f;
+            })
+        );
+        const observables = [obs1, obs3];
+
+        this.item.next(observables);
+    }
+
+    private init5() {
+        const obs1 = this.createNumberMarbles(0, 2000, 4000, new Marble(1, 100), new Marble(1, 7000), new Marble(2, 6000));
+        const obs2 = this.createCharMarbles(7000,  7000, 7000);
+
+        const obs3 = obs1.pipe(
+            pairwise(),
+            map(([m, n]) => {
+                const f = new Marble(m.text + '' + n.text, 0);
+                f.observableId = 3;
+                return f;
+            })
+        );
+        const observables = [obs1, obs3];
+
+        this.item.next(observables);
+    }
+
+    private init6() {
+        const obs1 = this.createNumberMarbles(0, 2000, 4000, new Marble(1, 100), new Marble(1, 7000), new Marble(2, 6000));
+        const obs2 = this.createCharMarbles(7000,  7000, 7000);
+
+        const obs3 = obs1.pipe(
+            repeat(3),
+            map(m => {
+                const f = new Marble(m.text, 0);
+                f.observableId = 3;
+                return f;
+            }),
+        );
+        const observables = [obs1, obs3];
 
         this.item.next(observables);
     }
